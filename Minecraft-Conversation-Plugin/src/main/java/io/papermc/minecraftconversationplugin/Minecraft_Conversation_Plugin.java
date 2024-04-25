040d72dcd949c8cc1e8420707eece04e73b5b6fe
sadge <137901464+sadge02@users.noreply.github.com>
2024-04-25 14:08:12 +0200

    Minecraft-Conversation-Plugin - 1.0.0
    Initiate the project

---
+++ Minecraft-Conversation-Plugin/src/main/java/io/papermc/minecraftconversationplugin/Minecraft_Conversation_Plugin.java
@@ -0,0 +1,17 @@
+package io.papermc.minecraftconversationplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraft_Conversation_Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
